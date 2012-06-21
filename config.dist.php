a56762edde85069198a61bbf843c072b73efb292
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-06-21 21:26:03 +0200

    config

--- config.dist.php
+++ config.dist.php
@@ -2,7 +2,7 @@
 
 $CONF = array(
 	"title" => "payutc",
-	"casper_url" => "http://89.88.36.152/scoobydoo/",
+	"scoobydoo_url" => "http://89.88.36.152/scoobydoo/",
 	"soap_url" => "http://89.88.36.152/server"
  	);
 
