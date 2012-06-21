86157bfebea2e44fd0df8a1264a130621a64b9e7
Matthieu Guffroy <mattgu74@gmail.com>
2012-06-21 20:48:48 +0200

    debut

---
+++ view.php
@@ -0,0 +1,43 @@
+<?
+
+$view=Array();
+$view["header"] = '
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">'.$CONF["title"].'</a>
+          <div class="btn-group pull-right">
+            <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+              <i class="icon-user"></i> Username
+              <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Profile</a></li>
+              <li class="divider"></li>
+              <li><a href="#">Sign Out</a></li>
+            </ul>
+          </div>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>';
+
+
+ $view["footer"] = '
+      <hr>
+
+      <footer>
+        <p>&copy; '.$CONF["title"].'</p>
+      </footer>
+ ';
