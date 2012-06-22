ffd6cdb7a9be3bb871c89063f87caa98f8a1ae1a
Matthieu Guffroy <mattgu74@gmail.com>
2012-06-23 00:54:22 +0200

    MADMIN sert à rien ici...

--- soap.php
+++ soap.php
@@ -1,6 +1,5 @@
 <?php
 
-$MADMIN = @new SoapClient($CONF['soap_url'].'/MADMIN.class.php?wsdl');
 $AADMIN = @new SoapClient($CONF['soap_url'].'/AADMIN.class.php?wsdl');
 
 
@@ -9,11 +8,9 @@ $AADMIN = @new SoapClient($CONF['soap_url'].'/AADMIN.class.php?wsdl');
 echo "<pre>";
 print_r($AADMIN->__getFunctions());
 
-print_r($AADMIN->getArray());
-print_r($AADMIN->getArray2());
-
-print_r($AADMIN->get_categories());
+print_r($AADMIN->get_categorie(125));
 print_r($AADMIN->get_articles());
 echo "</pre>";
 exit();
-// */
+//  */
+
