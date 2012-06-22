6c356193d93980eeac4cc02180f9b840b3e571a4
Matthieu Guffroy <mattgu74@gmail.com>
2012-06-22 19:42:33 +0200

    ModuleArticle action_mainjs

--- modules/article/ModuleArticle.class.php
+++ modules/article/ModuleArticle.class.php
@@ -18,7 +18,7 @@ class ModuleArticle extends Module {
 		$view->set_template('html');
 		$view->add_jsfile('libs/jquery-1.7.2.min.js');
 		$view->add_jsfile($modulepath.'res/js/jqtree.jquery.js');
-		$view->add_jsfile($modulepath.'res/js/main.js');
+		$view->add_jsfile($modulepath.'?module=article&action=mainjs');
 		$view->set_view($modulepath.'view/index.phtml');
 		return $view;
 	}
@@ -34,7 +34,24 @@ class ModuleArticle extends Module {
 				new Node(5, 'child3')
 			))
 		));
-		return $view;
+	}
+
+	public function action_mainjs(&$view) {
+		// Pour cette action on veut le template JS
+		$this->view->set_template("js");
+
+		// On veut égallement une vue particuliére
+		$myview = $this->view_to_filename("main.js");
+		$this->view->set_view($myview);
+
+		// Configuration des parametres nécessaires à la vue (les urls ajax)
+		global $CONF;
+		$url_base = $CONF["scoobydoo_url"]."?module=article&action=";
+		$this->view->add_param("get_tree", $url_base."get_tree");
+		$this->view->add_param("details_article", $url_base."details_article");
+		$this->view->add_param("details_categorie", $url_base."details_categorie");
+		$this->view->add_param("save_article", $url_base."save_article");
+		$this->view->add_param("save_categorie", $url_base."save_categorie");
 	}
 }
 
