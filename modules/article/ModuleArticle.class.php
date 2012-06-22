fdc5c4da24ce93759a661cf1a6add32b7e7b455e
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-06-23 00:40:46 +0200

    arbre des catégories et articles opérationnel

--- modules/article/ModuleArticle.class.php
+++ modules/article/ModuleArticle.class.php
@@ -24,16 +24,38 @@ class ModuleArticle extends Module {
 	}
 
 	public function action_get_tree() {
+		global $AADMIN;
 		$this->view->set_template('json');
-		$this->view->set_param(array(
-			new Node(1, 'node1', array(
-				new Node(2, 'child1'),
-				new Node(3, 'child2'),
-			)),
-			new Node(4, 'node2', array(
-				new Node(5, 'child3')
-			))
-		));
+		$arr = array(
+			array('id'=> 0, 'label'=>'label0', 'parent'=>NULL),
+			array('id'=> 1, 'label'=>'label1', 'parent'=>0),
+			array('id'=> 2, 'label'=>'label2', 'parent'=>0),
+			array('id'=> 3, 'label'=>'label3', 'parent'=>1),
+			array('id'=> 4, 'label'=>'label4', 'parent'=>1),
+		);
+		$temp = $AADMIN->get_categories();
+		$categories = array('root' => array('id'=>'root', 'label'=>'root', 'parent_id'=>null, 'children'=>array()));
+		foreach ($temp as $categorie) {
+			$categorie['children'] = array();
+			if (!$categorie['parent_id']) {
+				$categorie['parent_id'] = 'root';
+			}
+			$categories[$categorie['id']] = $categorie;
+		}
+		$articles = $AADMIN->get_articles();
+		foreach ($articles as $article) {
+			$categories[$article['parent_id']]['children'][] = $article;
+		}
+
+		//echo '<pre>';print_r($categories);echo '</pre>';
+		
+		$tree = $this::generate_tree($categories, 'parent_id');
+
+		//echo '<pre>';print_r($tree);echo '</pre>';
+
+		//die();
+
+		$this->view->set_param($tree);
 	}
 
 	public function action_article_details() {
@@ -96,6 +118,29 @@ class ModuleArticle extends Module {
 		$this->view->add_param('details_categorie', $url_base.'categorie_details');
 		$this->view->add_param('save_article', $url_base.'save_article');
 		$this->view->add_param('save_categorie', $url_base.'save_categorie');
+	}
+
+	
+	/**
+	 * A partir d'une array cré l'arbre hierarchisé associé, fonction récursive.
+	 *
+	 * @param $arr (array) contient la liste des éléments à hierarchisé,
+	 * 						ces éléments sont sous la forme clef=>array(champs1=>v1, champs2=>v2...)
+	 * @param $key_parent (string) le nom du champs qui contient l'id du parent
+	 * @param $parent le parent à utiliser comme base de l'arbre
+	 */
+	public static function generate_tree(array $arr, $key_parent, $parent = NULL) {
+		$tree = array();
+		foreach ($arr as $key=>&$object) {
+			if ($object[$key_parent] == $parent) {
+				unset($arr[$key]);
+				if (!$object['children']) {
+					$object['children'] = ModuleArticle::generate_tree($arr, $key_parent, $key);
+				}
+				$tree[] = $object;
+			}
+		}
+		return $tree;
 	}
 }
 
--- soap.php
+++ soap.php
@@ -1,3 +1,19 @@
 <?php
 
 $MADMIN = @new SoapClient($CONF['soap_url'].'/MADMIN.class.php?wsdl');
+$AADMIN = @new SoapClient($CONF['soap_url'].'/AADMIN.class.php?wsdl');
+
+
+// UNCOMMENT TO DEBUG SOAP
+/*
+echo "<pre>";
+print_r($AADMIN->__getFunctions());
+
+print_r($AADMIN->getArray());
+print_r($AADMIN->getArray2());
+
+print_r($AADMIN->get_categories());
+print_r($AADMIN->get_articles());
+echo "</pre>";
+exit();
+// */
