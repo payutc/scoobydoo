8458b44d52be56bd4eed53e2c3ed2bfa4cdb5f0c
Arthur Puyou <arthur@puyou.fr>
2012-10-27 01:39:08 +0100

    Supprimer le responsive

--- view.php
+++ view.php
@@ -12,7 +12,7 @@ class View {
 	public $view_url; // URL DE LA VUE A CHARGER
 	public $param = array(); // STOCKAGE DES PARAMETRES NECESAIRES AUX VUES
 	public $js_files = array('libs/jquery-1.7.2.min.js', 'bootstrap/bootstrap-dropdown.js'); // Url des fichiers javascipts à charger  !! Utiliser uniquement dans les templates html... (pas en json)
-	public $css_files = array('bootstrap/css/bootstrap.min.css','bootstrap/css/bootstrap-responsive.min.css'); // Url des fichiers css à charger  !! Utiliser uniquement dans les templates html... (pas en json)
+	public $css_files = array('bootstrap/css/bootstrap.min.css'); // Url des fichiers css à charger  !! Utiliser uniquement dans les templates html... (pas en json)
 	public $menus;
 	public $index_link;
 	public $copyright;
