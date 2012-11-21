768020976cc50ff1d70b6b83a8747f825d55d628
Patrick Browne <pt.browne@gmail.com>
2012-11-21 09:52:34 +0100

    added link to download csv report

--- modules/treso/ModuleTreso.class.php
+++ modules/treso/ModuleTreso.class.php
@@ -48,6 +48,42 @@ class ModuleTreso extends Module {
 		$this->view->add_param("summary", $summary);
 	}
 
+	protected function action_download() {
+		global $CONF;
+		global $AADMIN;
+		$this->view->set_template('csv');
+		$this->view->set_view($this->get_path_module()."view/stats.phtml");
+
+		if (isset($_GET["day"]) && isset($_GET["month"]) && isset($_GET["year"]) ) {
+			$this->view->add_param("day", $_GET["day"]);
+			$this->view->add_param("month", $_GET["month"]);
+			$this->view->add_param("year", $_GET["year"]);
+
+			if (isset($_GET["day2"]) && isset($_GET["month2"]) && isset($_GET["year2"]) ) {
+				$this->view->add_param("day2", $_GET["day2"]);
+				$this->view->add_param("month2", $_GET["month2"]);
+				$this->view->add_param("year2", $_GET["year2"]);
+
+				$summary = $AADMIN->get_summary_for_accounting_period(
+									intval($_GET["day"]), intval($_GET["month"]), intval($_GET["year"]),
+									intval($_GET["day2"]),intval($_GET["month2"]),intval($_GET["year2"]));
+
+			}
+			else {
+				$summary = $AADMIN->get_summary_for_accounting(intval($_GET["day"]), intval($_GET["month"]), intval($_GET["year"]));
+			}
+		}
+		else {
+			$now = mktime();
+			$this->view->add_param("day", date("d", $now));
+			$this->view->add_param("month", date("m", $now));
+			$this->view->add_param("year", date("Y", $now));
+			$summary = $AADMIN->get_summary_for_accounting(intval(date("Y")), intval(date("m")), intval(date("d")));
+		}
+		$this->view->add_param("summary", $summary);
+
+	}
+
 }
 
 ?>
